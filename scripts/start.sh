0f11718435246d4ffeff0222c243e676302f771b
LucasGaab <lucasgoliveiras@gmail.com>
2023-12-15 08:38:42 -0300

    edit scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 nomeApp="helloworld-react-lgabriel"
 cd /appDeploy
+npm cache clean -f
 npm install
 
+process=`sudo lsof -i :9001`
+pid=`echo $process | grep -oP '\b\d+\b' | head -n 1`
+sudo kill -9 $pid
+
 sudo supervisord
-sudo supervisorctl restart $nomeApp
+sudo supervisorctl start $nomeApp
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-exec > >(tee -a /home/ubuntu/appDeploy/logs/app/stdout/stop.log) 2>&1
-
-
 nomeApp="helloworld-react-lgabriel"
 
-sudo supervisorctl stop all
+sudo supervisorctl stop $nomeApp
