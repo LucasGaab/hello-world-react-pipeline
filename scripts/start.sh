2a2f310def4161ed0641c41d9bf4b5f07f56b34f
LucasGaab <lucasgoliveiras@gmail.com>
2023-12-18 10:58:32 -0300

    edit scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,5 +8,5 @@ process=`sudo lsof -i :9001`
 pid=`echo $process | grep -oP '\b\d+\b' | head -n 1`
 sudo kill -9 $pid
 
-sudo supervisord
-sudo supervisorctl start $nomeApp
+supervisord
+supervisorctl start $nomeApp
