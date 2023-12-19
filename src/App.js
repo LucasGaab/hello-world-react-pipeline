a272082a5eba1e8e4b61bd6fe2f6630fd0d449d6
LucasGaab <lucasgoliveiras@gmail.com>
2023-12-19 09:23:11 -0300

    pipeline test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Teste pipeline valcann - EC2
+          Teste pipeline valcann - EC2 test
         </p>
         <a
           className="App-link"
